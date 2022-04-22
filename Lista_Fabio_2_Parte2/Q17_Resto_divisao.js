8a577bc8ffabd76c3f79485aa15966d6497dfd59
KelsonF <kfeduardo2003@gmail.com>
2022-04-22 01:36:12 -0300

    confirmar

--- Lista_Fabio_2_Parte2/Q17_Resto_divisao.js
+++ Lista_Fabio_2_Parte2/Q17_Resto_divisao.js
@@ -1,5 +1,35 @@
 import { input,print } from "../io_utils.js";
 
 function main(){
-    const 
+    const valor1 = Number(input("Digite um valor inteiro: "))
+    const valor2 = Number(input("Digite um valor inteiro: "))
+
+    const resultado = operacao(valor1,valor2)
+
+    print(`Reultado:${resultado}`)
 }
+function operacao(v1,v2){
+    let operacoes_importantes
+    if(v1 % v2 === 1){
+        operacoes_importantes = ((v1 + v2)+1)
+    }
+    else if(v1 % v2 === 2){
+        if(v1%2 === 2 && v2%2 === 2){
+            operacoes_importantes = "Sao pares"
+        }
+        else{
+            operacoes_importantes = "Sao impares"
+        }
+    }
+    else if(v1 % v2 === 3){
+        operacoes_importantes = ((v1 + v2)* v1)
+    }
+    else if(v1 % v2 === 4){
+        operacoes_importantes = ((v1 + v2)/v2)
+    }
+    else{
+        operacoes_importantes = (v1**2),(v2**2)
+    }
+    return operacoes_importantes
+}
+main()
