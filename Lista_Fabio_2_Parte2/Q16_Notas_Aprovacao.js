715acc6aee29d10d6ec1904c96d2cd4425b8c733
KelsonF <kfeduardo2003@gmail.com>
2022-04-22 00:43:45 -0300

    escape

--- Lista_Fabio_2_Parte2/Q16_Notas_Aprovacao.js
+++ Lista_Fabio_2_Parte2/Q16_Notas_Aprovacao.js
@@ -4,7 +4,9 @@ function main(){
     const notas = input('Digite o valor das notas do aluno: ').split(' ')
     const notaA = Number(notas[0])
     const notaB = Number(notas[1])
+}
+function aprovado(n1,n2){
+    const media = (n1 + n2)/2
     
-    print(notaA,notaB)
 }   
 main()
