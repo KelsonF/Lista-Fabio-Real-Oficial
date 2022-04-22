c97bf678f424b90e2a0f3e42f7f3ea6054b461af
KelsonF <kfeduardo2003@gmail.com>
2022-04-22 01:39:44 -0300

    enter

--- Lista_Fabio_2_Parte2/Q17_Resto_divisao.js
+++ Lista_Fabio_2_Parte2/Q17_Resto_divisao.js
@@ -9,27 +9,25 @@ function main(){
     print(`Reultado:${resultado}`)
 }
 function operacao(v1,v2){
-    let operacoes_importantes
     if(v1 % v2 === 1){
-        operacoes_importantes = ((v1 + v2)+1)
+        return ((v1 + v2)+1)
     }
     else if(v1 % v2 === 2){
         if(v1%2 === 2 && v2%2 === 2){
-            operacoes_importantes = "Sao pares"
+            return "Sao pares"
         }
         else{
-            operacoes_importantes = "Sao impares"
+            return"Sao impares"
         }
     }
     else if(v1 % v2 === 3){
-        operacoes_importantes = ((v1 + v2)* v1)
+        return ((v1 + v2)* v1)
     }
     else if(v1 % v2 === 4){
-        operacoes_importantes = ((v1 + v2)/v2)
+        return ((v1 + v2)/v2)
     }
     else{
-        operacoes_importantes = (v1**2),(v2**2)
+       return (v1**2),(v2**2)
     }
-    return operacoes_importantes
 }
 main()
